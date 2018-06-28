41cce151d38c5a1954f0b99a6f0ce2f8fc37daeb
shouvik126 <35471259+shouvik126@users.noreply.github.com>
2018-06-28 10:55:22 +0530

    Add files via upload

---
+++ init.py
@@ -0,0 +1,10 @@
+class Tuna:
+    def __init__(self):
+        print('shouvik pradhan')
+    def __shouvik__(self):
+        print('pradhan')
+    def normal(self):
+        print('i m swimming')
+tt=Tuna()
+tt.normal()
+tt.__shouvik__()
